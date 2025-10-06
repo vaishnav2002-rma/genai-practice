af67140512e17a8e641b45eb273723efc33e0290
vaishnav2002-rma <vaishnavm.sudhish@thezennialpro.com>
2025-10-06 17:57:05 +0530

    Updated summarize text and generate entities to newer SDK.

--- fastapi_gemini.py
+++ fastapi_gemini.py
@@ -1,44 +1,61 @@
-import os 
-import json 
-import google.generativeai as genai 
-from fastapi import FastAPI, HTTPException 
-from pydantic import BaseModel 
-from dotenv import load_dotenv 
+import os
+import json
+from google import genai
+from google.genai import types
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from dotenv import load_dotenv
 
 load_dotenv()
 
-genai.configure(api_key = os.getenv("GEMINI_API_KEY"))
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-app = FastAPI(text = "Gemini text processing api")
+app = FastAPI(title="Gemini Text Processing API")
 
 class TextInput(BaseModel):
-    text: str 
+    text: str
 
 @app.post("/v1/summarize")
 def generate_summary(data: TextInput):
     try:
-        model = genai.GenerativeModel("gemini-2.5-flash")
-        prompt = f"Generate summary of the following text in 2-3 line:\n\n{data.text}"
-        response = model.generate_content(prompt)
+        prompt = f"Generate a summary of the following text in 2-3 lines:\n\n{data.text}"
+
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=prompt
+        )
+
         summary = response.text.strip()
-        return {"Summary":summary}
+        return {"Summary": summary}
+
     except Exception as e:
-        raise HTTPException(status_code=500,detail=str(e))
-    
+        raise HTTPException(status_code=500, detail=str(e))
+
+
 @app.post("/v1/entities")
 def generate_entities(data: TextInput):
     try:
-        model = genai.GenerativeModel("gemini-2.5-flash")
         prompt = f"""
         Extract and generate entities from the text below.
-        Generate the entities in json format and include the fields entity, type, context.
+        Generate the entities in JSON format and include the fields: entity, type, and context.
+        DO NOT include any markdown formatting (like ```json) or any surrounding text — only the raw JSON array.
 
         Text:
         {data.text}
         """
-        response = model.generate_content(prompt)
+
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=prompt
+        )
+
         json_string = response.text.strip()
+
         entities_data = json.loads(json_string)
+
         return {"Entities": entities_data}
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+

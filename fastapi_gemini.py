e3b477cb9ed093a865010cf64634dda0d549fdfa
vaishnav2002-rma <vaishnavm.sudhish@thezennialpro.com>
2025-10-07 12:37:54 +0530

    Added chain of thought and pdf handling endpoints and also made the asynchronous

--- fastapi_gemini.py
+++ fastapi_gemini.py
@@ -1,10 +1,13 @@
 import os
 import json
+import asyncio
 from google import genai
 from google.genai import types
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import PlainTextResponse
 from pydantic import BaseModel
 from dotenv import load_dotenv
+import httpx 
 
 load_dotenv()
 
@@ -16,11 +19,12 @@ class TextInput(BaseModel):
     text: str
 
 @app.post("/v1/summarize")
-def generate_summary(data: TextInput):
+async def generate_summary(data: TextInput):
     try:
         prompt = f"Generate a summary of the following text in 2-3 lines:\n\n{data.text}"
 
-        response = client.models.generate_content(
+        response = await asyncio.to_thread(
+            client.models.generate_content,
             model="gemini-2.0-flash",
             contents=prompt            
         )
@@ -33,7 +37,7 @@ def generate_summary(data: TextInput):
 
 
 @app.post("/v1/entities")
-def generate_entities(data: TextInput):
+async def generate_entities(data: TextInput):
     try:
         prompt = f"""
         Extract and generate entities from the text below.
@@ -44,9 +48,10 @@ def generate_entities(data: TextInput):
         {data.text}
         """
 
-        response = client.models.generate_content(
+        response = await asyncio.to_thread(
+            client.models.generate_content,
             model="gemini-2.0-flash",
-            contents=prompt
+            contents=prompt            
         )
 
         json_string = response.text.strip()
@@ -58,35 +63,58 @@ def generate_entities(data: TextInput):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-
 @app.post("/v1/few_shot")
-def few_shot_prompting():
-    prompt="""
-    Sort the club based on the most chapions league trophies
-    Question: Ajax, PSG, Manchester United
-    Answer: Ajax(4), Manchester United(3), PSG(1)
-    Question: Real Madrid, Liverpool, AC Milan
-    Answer: 
-    """
+async def few_shot_prompting(data: TextInput):
+    prompt=f"{data.text}"
 
-    response = client.models.generate_content(
-        model = "gemini-2.5-flash",
-        contents = prompt
-    )
+    response = await asyncio.to_thread(
+            client.models.generate_content,
+            model="gemini-2.0-flash",
+            contents=prompt            
+        )
 
-    return response.text
+    return response.text.strip()
 
 @app.post("/v1/zero_shot")
-def few_shot_prompting():
-    prompt="""
-    Sort the club based on the most league trophies. Return the number of league titles in brackets beside the team name. Don't give space and just space the teams with comma.
-    Question: Real Madrid, PSG, Manchester United
-    Answer: 
-    """
+async def zero_shot_prompting(data: TextInput):
+    prompt=f"{data.text}"
 
-    response = client.models.generate_content(
-        model = "gemini-2.5-flash",
-        contents = prompt
-    )
+    response = await asyncio.to_thread(
+            client.models.generate_content,
+            model="gemini-2.0-flash",
+            contents=prompt            
+        )
 
-    return response.text
+    return response.text.strip()
+
+@app.post("/v1/reasoning")
+async def chain_of_thought(data:TextInput):
+    prompt =f"{data.text}"
+
+    response = await asyncio.to_thread(
+            client.models.generate_content,
+            model="gemini-2.0-flash",
+            contents=prompt            
+        )
+
+    return response.text.strip()
+
+@app.post("/v1/file_summary",response_class=PlainTextResponse)
+async def generate_pdf_summary():
+    doc_url = "https://arxiv.org/pdf/2203.02155"
+    async with httpx.AsyncClient() as async_client:
+        doc_response = await async_client.get(doc_url)
+        doc_data = doc_response.content
+
+    prompt = "Summarize the document"
+    response = await asyncio.to_thread(
+        client.models.generate_content,
+        model="gemini-2.5-flash",
+        contents = [
+        types.Part.from_bytes(
+            data=doc_data,
+            mime_type='application/pdf',
+        ),
+        prompt])
+    
+    return response.text.strip()
